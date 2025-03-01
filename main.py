6fde583467340f2c9b216fb5e1ffd675184f8b1f
nagervadzenika81 <nagervadze.98@mail.ru>
2025-03-01 12:29:20 +0400

    main.py

--- main.py
+++ main.py
@@ -84,3 +84,9 @@ def generate_report():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
